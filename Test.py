49949f8a9e5e214dc2534c3ea3051bc4999f45ed
Andrew Nakamoto <andrewsnakamoto@gmail.com>
2023-11-26 16:06:19 -0800

    Created a test python file

---
+++ Test.py
@@ -0,0 +1,15 @@
+"""
+Module Docstring
+"""
+
+__author__ = "Andrew Nakamoto"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
