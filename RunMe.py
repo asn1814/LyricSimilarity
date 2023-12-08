514aee0f5b0ede04ca291892930b0ffde00382aa
Andrew Nakamoto <andrewsnakamoto@gmail.com>
2023-12-08 14:08:39 -0800

    Basic LDA implementation

--- RunMe.py
+++ RunMe.py
@@ -6,6 +6,8 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import nltk
+import gensim
 #from sklearn.decomposition import KernelPCA
 
 from collections import Counter
@@ -20,9 +22,14 @@ SONGMETADATA: list = ["Artist", "Title", LYRICS]
 def main():
     """ Main entry point of the app """
 
-    """# get a pandas dataframe
+    # get a pandas dataframe
     SongsCSVDF: pd.DataFrame = getDataFrameSongsCSV()
+    
+    #useBagOfWordsPCA(SongsCSVDF)
 
+    useLDA(SongsCSVDF)
+
+def useBagOfWordsPCA(SongsCSVDF: pd.DataFrame):
     # build the corpus
     corpus: [] = buildCorpus(SongsCSVDF)
     
@@ -33,11 +40,49 @@ def main():
     # can take a while due to buildRelativeOccurances, so do it once and then use the saved csv
     mostCommonWords: dict = dict(wordcounts.most_common(NMOSTCOMMON))
     relativeDF = buildRelativeOccurances(mostCommonWords, SongsCSVDF)
-    relativeDF.to_csv(PROCESSEDCSVFILEPATH, index=False, encoding="utf-8")"""
+    relativeDF.to_csv(PROCESSEDCSVFILEPATH, index=False, encoding="utf-8")
 
     relativeDF: pd.DataFrame = pd.read_csv(PROCESSEDCSVFILEPATH, encoding="utf-8")
 
     plotPCA(relativeDF)
+
+
+def useLDA(SongsCSVDF: pd.DataFrame): 
+    # https://rstudio-pubs-static.s3.amazonaws.com/79360_850b2a69980c4488b1db95987a24867a.html
+    # basic processing on the lyrics
+    lyrics_set = list(SongsCSVDF[LYRICS])
+    processed_lyrics = list()
+    for elt in lyrics_set:
+        processed_lyrics.append(removeStopsAndStem(tokenize(elt)))
+
+    # The Dictionary() function traverses texts, assigning a unique integer id to each unique token while also collecting word counts and relevant statistics. To see each token’s unique integer id, try print(dictionary.token2id).
+    dictionary = gensim.corpora.Dictionary(processed_lyrics)
+
+    # The doc2bow() function converts dictionary into a bag-of-words. The resulting corpus is a list of vectors equal to the number of documents.
+    corpus = [dictionary.doc2bow(text) for text in processed_lyrics]
+
+    # The LdaModel class is described in detail in the gensim documentation.
+    print("Beginning LDA training")
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=8, id2word = dictionary, passes=20)
+    print("LDA model trained")
+
+    # Our LDA model is now stored as ldamodel. We can review our topics with the print_topic and print_topics methods
+    print(ldamodel.print_topics(num_topics=-1, num_words=8))
+
+
+
+def removeStopsAndStem(tokens: list) -> list:
+    stops = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+    stops.append("urlcopyembedcopy")
+
+    stemmer = nltk.stem.PorterStemmer()
+    
+    result = []
+    for token in tokens:
+        if token not in stops and token.find("embedshare") == -1:
+            #result.append(stemmer.stem(token)) # use the stemmer
+            result.append(token) # dont use the stemmer
+    return result
 
 
 def plotPCA(df: pd.DataFrame):
